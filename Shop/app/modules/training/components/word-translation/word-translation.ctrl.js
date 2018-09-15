21bbb47ba5de6e991a6173ed30c81c8f72effce7
Alckevich <alckevich@live.com>
2018-09-16 00:08:49 +0300

    results page

--- Shop/app/modules/training/components/word-translation/word-translation.ctrl.js
+++ Shop/app/modules/training/components/word-translation/word-translation.ctrl.js
@@ -1,36 +1,46 @@
 ﻿export default class {
-    static get $inject() {
-        return [
-            '$timeout',
-            'trainingService'
-        ];
-    }
+	static get $inject() {
+		return [
+			'$timeout',
+			'trainingService',
+			'spinnerService'
+		];
+	}
 
-    constructor(
-        $timeout,
-        /*Service*/ training
-    ) {
-        this.$timeout = $timeout;
-        this.training = training;
+	constructor(
+		$timeout,
+        /*Service*/ training,
+        /*Service*/ spinner,
+	) {
+		this.$timeout = $timeout;
+		this.training = training;
+		this.spinner = spinner;
 
-        this.wait = false;
-    }
+		this.wait = false;
+	}
 
-    hint() {
-        this.submit(null);
-    }
+	hint() {
+		this.submit(null);
+	}
 
-    submit(option) {
-        if (this.wait) {
-            return;
-        }
+	submit(option) {
+		if (this.wait) {
+			return;
+		}
 
-        this.wait = true;
-        this.training.submit(option);
-        this.$timeout(() => {
-            this.training.next();
-            this.wait = false;
-            this.training.finish && this.training.save();
-        }, 500);
-    }
+		this.wait = true;
+		this.training.submit(option);
+		this.$timeout(() => {
+			this.training.next();
+			this.wait = false;
+			this.training.finish && this.training.save();
+		}, 500);
+	}
+
+	play() {
+		this.spinner.show();
+		this.training
+			.load()
+			.finally(() => this.spinner.hide());
+	}
 }
